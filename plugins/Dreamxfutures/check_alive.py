e68ad9be4b5696076cae8258f008ba6394a95f60
mimamofficial1 <muzaffarimamneema@gmail.com>
2026-06-23 23:19:28 +0530

    Update check_alive.py

--- plugins/Dreamxfutures/check_alive.py
+++ plugins/Dreamxfutures/check_alive.py
@@ -22,7 +22,7 @@ CMD = ["/", "."]
 
 @Client.on_message(filters.command("alive", CMD))
 async def check_alive(_, message):
-    sticker = await message.reply_sticker("CAACAgIAAxkBAAEBVAlmCYqbLub_o5pVUOEwbqhV8kRytgACRBkAAgjh2UlSqev16oISqB4E") 
+    sticker = await message.reply_sticker("CAACAgUAAxkBAAERb_1qOk0RcUM0FLSjAAEwRBNtKUYVJ0AAAp8ZAALZi1FXHOU2_Un69bw8BA") 
     text = await message.reply_text("Yᴏᴜ ᴀʀᴇ ᴠᴇʀʏ ʟᴜᴄᴋʏ 🤞 I ᴀᴍ ᴀʟɪᴠᴇ ❤️\nPʀᴇss /start ᴛᴏ ᴜsᴇ ᴍᴇ!")
     await asyncio.sleep(60)
     await sticker.delete()
