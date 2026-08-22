a4a7d65ab8bd37ef59ee05e5545bc683b43004be
mimamofficial1 <muzaffarimamneema@gmail.com>
2026-08-22 17:23:03 +0530

    Add files via upload

--- info.py
+++ info.py
@@ -137,6 +137,7 @@ MAX_B_TN = environ.get("MAX_B_TN", "5") # Maximum number of buttons in a row (de
 PORT = int(environ.get("PORT", "8080"))  # Port for the web server (default: 8080)
 MSG_ALRT = environ.get('MSG_ALRT', 'Share & Support Us ♥️') # Alert message for users
 DELETE_TIME = int(environ.get("DELETE_TIME", "300"))  #  deletion time in seconds (default: 5 minutes). Adjust as per your needs.
+AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "3600"))  # default auto-delete time for group results in seconds (1 hour). Cycled between 1/2/3 hours from settings menu.
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")   # Custom caption for files
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION) # Custom caption for batch files
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", f"{script.IMDB_TEMPLATE_TXT}")     # Custom IMDB template 
